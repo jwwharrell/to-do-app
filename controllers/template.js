4947a1dc5f29083ec976a08055da789a15f57aba
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-22 13:12:27 -0500

    Made model for item

--- controllers/template.js
+++ controllers/template.js
@@ -15,7 +15,7 @@ const express = require('express')
  * controller you need.
  * 
  */
-const templateApi = require('../models/template.js')
+const templateApi = require('../models/toDoItem/item.js/index.js')
 
 /* Step 3 
  * 
---
+++ models/item/item.js
@@ -0,0 +1,37 @@
+const mongoose = require('./connection.js')
+
+const ItemModelSchema = new mongoose.Schema({
+  name: String,
+  description: String
+})
+
+const ItemCollection = mongoose.model('Item', ItemModelSchema)
+
+
+const getAllItems = () => {
+  return ItemCollection.find({})
+}
+
+const getItemById = (itemId) => {
+  return ItemCollection.findById(itemId)
+}
+
+const addNewItem = (newItem) => {
+  return ItemCollection.create(newItem)
+}
+
+const updateCurrentItem = (itemId, updatedItem) => {
+  return ItemCollection.updateOne({ _id: itemId }, updatedItem)
+} 
+
+const deleteCurrentItem = (itemId) => {
+  return ItemCollection.deleteOne({ _id: invoiceId })
+}
+
+module.exports = {
+  getAllItems,
+  getItemById,
+  addNewItem,
+  updateCurrentItem,
+  deleteCurrentItem
+}
