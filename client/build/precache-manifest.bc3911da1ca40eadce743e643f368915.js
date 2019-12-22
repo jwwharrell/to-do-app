6a72ccb58505e2d7c65244a447513b301fd3aa5d
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-22 12:51:54 -0500

    npm installed

---
+++ client/build/precache-manifest.bc3911da1ca40eadce743e643f368915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b40d771bbbc58c4a3d80b31c28e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49408be692f0bbc6332",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "c49408be692f0bbc6332",
+    "url": "/static/js/main.fe38c1eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
