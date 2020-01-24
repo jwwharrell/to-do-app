c8626b53e629c62f0843b4be42ea3cb35e63969c
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2020-01-23 19:36:58 -0500

    Items get marked as complete

--- client/src/components/EveryItem.js
+++ client/src/components/EveryItem.js
@@ -33,12 +33,14 @@ export default class EveryItem extends Component {
     }
 
     onFinishedClick = () => {
+        console.log()
         const value = this.state.currentItem.finished
         const previousState = { ...this.state }
         previousState.currentItem.finished = !value
         this.setState(previousState)
         const itemId = this.state.currentItem._id
         axios.put(`/api/item/${itemId}`, this.state.currentItem)
+        .then(this.refreshItem)
     }
 
     onRemoveClick = () => {
@@ -70,10 +72,9 @@ export default class EveryItem extends Component {
                             {this.state.itemList.map((item) => {
                                 const refLink = `#list-${item._id}`
                                 return (
-                                    <a key={item._id} id={item._id} onClick={this.onTaskSelection} className='list-group-item list-group-item-action' href={refLink} data-toggle="list" role="tab" >
-                                        {item.name}
-                                    </a>
-
+                                        <a key={item._id} id={item._id} onClick={this.onTaskSelection} className='list-group-item list-group-item-action' href={refLink} data-toggle="list" role="tab" >
+                                            {item.finished ? "Complete" : item.name}
+                                        </a>
                                 )
                             })}
                         </div>
@@ -82,13 +83,13 @@ export default class EveryItem extends Component {
                         <div className="tab-content" id="nav-tabContent">
                             {description ?
                                 <div className="tab-pane fade show active" role="tabpanel">
-                                        <Button variant={finished ? "outline-dark" : "outline-success"} size='sm' onClick={this.onFinishedClick} >{finished ? "Make Active Again" : "Mark As Done"}</Button>
-                                        <br />
-                                        <br />
-                                        {description}
-                                        <br />
-                                        <br />
-                                        <Button variant="outline-danger" size='sm' onClick={this.onRemoveClick}>Remove</Button>
+                                    <Button variant={finished ? "outline-dark" : "outline-success"} size='sm' onClick={this.onFinishedClick} >{finished ? "Make Active Again" : "Mark As Done"}</Button>
+                                    <br />
+                                    <br />
+                                    {description}
+                                    <br />
+                                    <br />
+                                    <Button variant="outline-danger" size='sm' onClick={this.onRemoveClick}>Remove</Button>
                                 </div>
                                 : null}
                         </div>
